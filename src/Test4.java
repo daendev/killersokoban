f2af7673ab38b29bb68aa4c86cef6a4dcafb84fd
Akster <soros.david.009@gmail.com>
2018-03-19 01:11:22 +0100

    Teszt fix 2-8

--- src/Test4.java
+++ src/Test4.java
@@ -1,17 +1,24 @@
-import sokoban.Cell;
-import sokoban.Directions;
-import sokoban.Hole;
-import sokoban.Player;
+import sokoban.*;
 
 public class Test4 {
     public static void run(){
+        Warehouse Warehouse_1 = new Warehouse("Warehouse_1");
         Cell Cell_1 = new Cell("Cell_1");
         Hole Hole_1 = new Hole("Hole_1");
         Player Player_1 = new Player("Player_1");
+
+        Cell_1.setWarehouse(Warehouse_1);
+        Hole_1.setWarehouse(Warehouse_1);
+        Player_1.setWarehouse(Warehouse_1);
+
+        Warehouse_1.addPlayer(Player_1);
+
         Player_1.setPlace(Cell_1);
         Cell_1.setHolding(Player_1);
+
         Cell_1.setNeighbour(Hole_1, Directions.right);
         Hole_1.setNeighbour(Cell_1, Directions.left);
+
         Player_1.move(Directions.right, Player_1);
     }
 }
--- src/Test7.java
+++ src/Test7.java
@@ -2,19 +2,32 @@ import sokoban.*;
 
 public class Test7 {
     public static void run(){
+        Warehouse Warehouse_1 = new Warehouse("Warehouse_1");
         Cell Cell_1 = new Cell("Cell_1");
         Cell Cell_2 = new Cell("Cell_2");
         Hole Hole_1 = new Hole("Hole_1");
         Player Player_1 = new Player("Player_1");
         Box Box_1 = new Box("Box_1");
+
+        Cell_1.setWarehouse(Warehouse_1);
+        Cell_2.setWarehouse(Warehouse_1);
+        Hole_1.setWarehouse(Warehouse_1);
+        Player_1.setWarehouse(Warehouse_1);
+        Box_1.setWarehouse(Warehouse_1);
+
+        Warehouse_1.addPlayer(Player_1);
+        Warehouse_1.addBox(Box_1);
+
         Player_1.setPlace(Cell_1);
         Cell_1.setHolding(Player_1);
         Box_1.setPlace(Cell_2);
         Cell_2.setHolding(Box_1);
+
         Cell_1.setNeighbour(Cell_2, Directions.right);
         Cell_2.setNeighbour(Cell_1, Directions.left);
         Cell_2.setNeighbour(Hole_1, Directions.right);
         Hole_1.setNeighbour(Cell_2, Directions.left);
+
         Player_1.move(Directions.right, Player_1);
     }
 }
