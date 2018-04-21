673f236fd4e46f004c0571a11ef903beb543fd5c
Kovács Levente <kovacslevi0@gmail.com>
2018-04-21 19:46:39 +0200

    read commands

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ import sokoban.*;
 public class Main {
 
     public static void main(String[] args) {
-        Warehouse w = new Warehouse();
-        w.generateMap(20);
-        w.draw();
+        //Warehouse w = new Warehouse();
+        //w.generateMap(20);
+        //w.draw();
+        Test t = new Test();
+        t.readCommand();
     }
 }
