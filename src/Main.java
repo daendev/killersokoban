abec1bfe3ca4a8f7403db5c229decca51d822870
akster <soros.david.009@gmail.com>
2018-04-23 09:40:45 +0200

    Init app

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
 import sokoban.*;
 
-
 public class Main {
 
     public static void main(String[] args) {
+        String load = "";
+        for(int i = 0; i<args.length; i++){
+            switch (args[i]){
+                case "-in":
+                    load = args[i++];
+                    break;
+                case "-out":
+                    break;
+                case "-log":
+                    break;
+                default:
+                    System.out.println("Wrong argument: " + args[i]);
+                    break;
+            }
+        }
         Warehouse w = new Warehouse();
         Test t = new Test(w);
         boolean exit = false;
+        if(!load.equals("")) t.load(load);
         while(!exit){
             t.readCommand();
             exit = t.executeCommand();
